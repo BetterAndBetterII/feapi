33df319884abf4359464af716ad1b315d63e4c71
Yuzhong Zhang <141388234+BetterAndBetterII@users.noreply.github.com>
2025-03-24 09:43:58 +0800

    update predownload

--- pre_download.py
+++ pre_download.py
@@ -1,5 +1,5 @@
-from sentence_transformers import SentenceTransformer
+from FlagEmbedding import BGEM3FlagModel
 
 if __name__ == "__main__":
-    model = SentenceTransformer("BAAI/bge-m3", trust_remote_code=True)
+    model = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True)
     model.encode("Hello, world!")
